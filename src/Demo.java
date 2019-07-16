412a7844c8b8dd4a7c3483dc7d0c36e7c5726f4f
wzb <2413640144@qq.com>
2019-07-16 19:58:39 +0800

    wzb

--- src/Demo.java
+++ src/Demo.java
@@ -3,6 +3,9 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println(123);
 		System.out.println(456);
+		System.out.println("yw");
+		System.out.println("sx");
+		System.out.println("yy");
 		
 	}
 
