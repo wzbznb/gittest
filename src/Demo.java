d077472be60fa68c02f54cb720c22728ab12ce6e
wzb <2413640144@qq.com>
2019-07-16 19:55:46 +0800

    wzb

--- src/Demo.java
+++ src/Demo.java
@@ -3,6 +3,10 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println(123);
 		System.out.println(456);
+		System.out.println(789);
+		System.out.println("wzb");
+		System.out.println("znb");
+		System.out.println("ddd");
 		
 	}
 
