24ab8bf909ca8a2e18f0d42064791ef52e866487
wzb <2413640144@qq.com>
2019-07-17 23:23:45 +0800

    wzb

--- src/Demo.java
+++ src/Demo.java
@@ -3,15 +3,12 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println(123);
 		System.out.println(456);
-
 		System.out.println(789);
 		System.out.println("wzb");
 		System.out.println("znb");
-
 		System.out.println("yw");
 		System.out.println("sx");
-		System.out.println("yy");
-		
+		System.out.println("yy");		
 		System.out.println("test1");
 		System.out.println("test2");
 		System.out.println("test3");
