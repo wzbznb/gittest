e95fe0c1b80927bac8cbf56b7bdbaaf2061c5e4c
wzbznb <2413640144@qq.com>
2019-11-21 14:58:15 +0800

    1458

--- src/efunction/Eeclass.java
+++ src/efunction/Eeclass.java
@@ -3,11 +3,6 @@ package efunction;
 public class Eeclass {
 public static void main(String[] args) {
 	System.out.println(1111);
-	System.out.println(1111);
-	System.out.println(1111);
-	System.out.println(1111);
-	System.out.println(1111);
-	System.out.println(1111);
 	System.out.println(2222);
 }
 }
